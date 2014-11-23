2709a89ef134bcb5f38b90b44ed212cb2d6f450c
mikan <yutakakato@icloud.com>
2014-11-23 23:47:04 +0900

    Initialized with netbeans.

---
+++ src/com/tasktoys/java8ws/Main.java
@@ -0,0 +1,12 @@
+package com.tasktoys.java8ws;
+
+/**
+ * 特に何もしないプロジェクト用メインクラスです。
+ *
+ * @author mikan
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is the main class.");
+    }
+}

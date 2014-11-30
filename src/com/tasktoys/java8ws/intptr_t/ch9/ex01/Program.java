ecfe22cb3e3d6048a9c9a521a3ccf6c537026d0d
intptr_t <niwatho.twi@gmail.com>
2014-11-30 21:08:10 +0900

    exercises09-01

--- src/com/tasktoys/java8ws/intptr_t/ch9/ex01/Program.java
+++ src/com/tasktoys/java8ws/intptr_t/ch9/ex01/Program.java
@@ -1,11 +1,31 @@
 package com.tasktoys.java8ws.intptr_t.ch9.ex01;
 
+import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 /**
- * 
+ * Javaプログラマーなら習得しておきたい Java SE 8 実践プログラム
+ * 第9章 練習問題1
  * @author intptr_t
  */
 public class Program {
-	public static void main(String[] args) {
-		System.out.println("hello,world!");
+	public static void main(String[] args) throws Exception {
+		Scanner in = null;
+		PrintWriter out = null;
+		try {
+			in = new Scanner(Paths.get("/usr/share/dict/words"));
+			out = new PrintWriter("/tmp/out.txt");
+
+			while(in.hasNext()) {
+				out.println(in.next().toLowerCase());
+			}			
+		} finally {
+			try {
+				if (in != null) { in.close(); }
+			} finally {
+				if (out != null){ out.close(); }				
+			}
+		}
 	}
 }

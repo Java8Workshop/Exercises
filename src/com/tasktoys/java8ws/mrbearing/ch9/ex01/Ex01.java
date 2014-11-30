896d8718a7e23a1a3af220f0290b942ce975a373
takumi Okamoto <takumi1988okamoto@gmail.com>
2014-11-30 21:30:19 +0900

    Ex01

--- src/com/tasktoys/java8ws/mrbearing/ch9/ex01/Ex01.java
+++ src/com/tasktoys/java8ws/mrbearing/ch9/ex01/Ex01.java
@@ -1,5 +1,9 @@
 package com.tasktoys.java8ws.mrbearing.ch9.ex01;
 
+import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Ex01 {
 
   public Ex01() {
@@ -7,6 +11,22 @@ public class Ex01 {
 
   public static void main(String[] args) {
     System.out.println("test");
+    
+    
+    
+    try{
+      Scanner in = new Scanner( Paths.get("./README.md") );
+      PrintWriter out = new PrintWriter("./outMrBearing.txt");
+      while(in.hasNext())
+        out.println(in.next().toLowerCase());
+      
+      in.close();
+      out.close();
+      
+    }catch(Exception e){
+      
+    }
+    
   }
 
 }

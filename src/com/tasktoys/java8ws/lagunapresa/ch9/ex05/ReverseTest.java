3ca12397a2c1d22cded76754460c8cd7ab1980e3
LagunaPresa <LagunaPresa@192.168.10.102>
2014-12-14 22:19:35 +0900

    added ex05

---
+++ src/com/tasktoys/java8ws/lagunapresa/ch9/ex05/ReverseTest.java
@@ -0,0 +1,12 @@
+package com.tasktoys.java8ws.lagunapresa.ch9.ex05;
+
+import org.junit.Test;
+
+public class ReverseTest {
+
+	@Test
+	public void test() throws Exception {
+		Reverse.main(new String[]{});
+	}
+
+}

f634b4c2ff411edfb290e835a5db628d96e733cc
YuichiroSato <physics.for.high.school.students@gmail.com>
2014-12-14 23:02:48 +0900

    Chapter9 Ex07

--- src/com/tasktoys/java8ws/sato/ch9/ex07/ReadURL.java
+++ src/com/tasktoys/java8ws/sato/ch9/ex07/ReadURL.java
@@ -1,17 +1,17 @@
 package com.tasktoys.java8ws.sato.ch9.ex07;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class ReadURL {
 
 	public static void main(String[] args) {
 		try {
-			URL url = new URL("http://anond.hatelabo.jp/");
-			url.openStream();
-			
+			URL url = new URL("http://anond.hatelabo.jp/20141214222840");
+			Files.copy(url.openStream(), Paths.get("./out/anond.txt"), StandardCopyOption.REPLACE_EXISTING);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}

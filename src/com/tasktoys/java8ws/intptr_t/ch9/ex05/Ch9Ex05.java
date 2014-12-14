84417e576c961d1eb639871974e7e7e69d14e593
intptr_t <niwatho.twi@gmail.com>
2014-12-14 22:10:13 +0900

    chapter9-5

--- src/com/tasktoys/java8ws/intptr_t/ch9/ex05/Ch9Ex05.java
+++ src/com/tasktoys/java8ws/intptr_t/ch9/ex05/Ch9Ex05.java
@@ -1,16 +1,20 @@
 package com.tasktoys.java8ws.intptr_t.ch9.ex05;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class Ch9Ex05 {
+	static final Path FILE_PATH = Paths.get("./out/intptr_t-09-05.txt");
+	
+	public static void main(String[] args) throws IOException {
+		byte[] bytes = Files.readAllBytes(Paths.get("./README.md"));
 
-	public static void main(String[] args) {
-		Path home = Paths.get("").toAbsolutePath();
-		
-		System.out.println(home);
-		System.out.println(home.resolve("../"));
-		System.out.println(home.resolve("/Users"));
+		Files.write(FILE_PATH, new byte[]{}, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+		for( int i = bytes.length - 1; i >= 0; i--) {
+			Files.write(FILE_PATH, new byte[]{bytes[i]}, StandardOpenOption.APPEND);
+		}
 	}
 }

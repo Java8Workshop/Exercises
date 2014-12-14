b4595a79d4c128bc675d895233949d5b6a235547
intptr_t <niwatho.twi@gmail.com>
2014-12-14 23:27:17 +0900

    update chapter9-6

--- src/com/tasktoys/java8ws/intptr_t/ch9/ex06/Ch9Ex06.java
+++ src/com/tasktoys/java8ws/intptr_t/ch9/ex06/Ch9Ex06.java
@@ -5,7 +5,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.Collections;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Ch9Ex06 {
 	static final Path FILE_PATH = Paths.get("./out/intptr_t-09-06.txt");
@@ -14,9 +16,11 @@ public class Ch9Ex06 {
 		List<String> lines = Files.readAllLines(Paths.get("./README.md"));
 
 		Files.write(FILE_PATH, new byte[]{}, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
-		for(int i = lines.size() - 1; i >= 0; i--) {
+
+		ListIterator<String> it = lines.listIterator(lines.size());
+		while( it.hasPrevious() ) {
 			StringBuffer buffer = new StringBuffer();
-			buffer.append(lines.get(i)).append("\n");
+			buffer.append(it.previous()).append("\n");
 
 			Files.write(FILE_PATH, buffer.toString().getBytes(), StandardOpenOption.APPEND);
 		}

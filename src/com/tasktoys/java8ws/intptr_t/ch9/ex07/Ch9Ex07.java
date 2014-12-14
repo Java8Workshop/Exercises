4f33664f04348dfe218d05043fc095d03222c8b7
intptr_t <niwatho.twi@gmail.com>
2014-12-14 22:45:50 +0900

    chapter9-7

---
+++ src/com/tasktoys/java8ws/intptr_t/ch9/ex07/Ch9Ex07.java
@@ -0,0 +1,18 @@
+package com.tasktoys.java8ws.intptr_t.ch9.ex07;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class Ch9Ex07 {
+	public static void main(String[] args) throws IOException {
+		String filePath = new File(".").getAbsolutePath() + "/README.md";
+		URL url = new URL( "file://" + filePath );
+		InputStream istream = url.openStream();
+		Files.copy(istream, Paths.get("./out/intptr_t-09-07.txt"), StandardCopyOption.REPLACE_EXISTING);
+	}
+}

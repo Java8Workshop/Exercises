fa1ba08c83d257405135b0b85c33d127d484598d
mikan <yutakakato@icloud.com>
2014-12-29 05:18:51 +0900

    Added ch09-ex12.

---
+++ src/com/tasktoys/java8ws/mikan/ch9/ex12/Launcher.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright(C) 2014 Java 8 Workshop participants. All rights reserved.
+ * https://github.com/Java8Workshop/About
+ */
+package com.tasktoys.java8ws.mikan.ch9.ex12;
+
+import com.tasktoys.java8ws.mikan.ch9.ex12.ui.fx.CreditCardFinderApplication;
+import javafx.application.Application;
+
+/**
+ * Launch JavaFX Application.
+ *
+ * @author mikan
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+        Application.launch(CreditCardFinderApplication.class);
+    }
+}

7df1e19607ed6d69c3aab7df56128dfbe1dc8e8e
intptr_t <niwatho.twi@gmail.com>
2015-01-04 21:07:08 +0900

    chapter9-11

---
+++ src/com/tasktoys/java8ws/intptr_t/ch9/ex11/Ch9Ex11.java
@@ -0,0 +1,22 @@
+package com.tasktoys.java8ws.intptr_t.ch9.ex11;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.TimeUnit;
+
+public class Ch9Ex11 {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		// サンプルファイルを生成
+		String credit = "0123-4567-8901-2345";
+		Files.write(Paths.get("./out/credit.txt"),
+					credit.getBytes(),
+					StandardOpenOption.CREATE,StandardOpenOption.TRUNCATE_EXISTING);
+		// メイン処理
+		ProcessBuilder processBuilder = new ProcessBuilder("grep","-r","\\([0-9]\\{4\\}[-| ]\\?\\)\\{3\\}[0-9]\\{4\\}", "./out");
+		processBuilder.inheritIO();
+		Process process = processBuilder.start();
+		process.waitFor(60L,TimeUnit.SECONDS);
+	}
+}

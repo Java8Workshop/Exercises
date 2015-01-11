3fdf85a01590291793eb8d62c6ce255c6f66925c
takumi Okamoto <takumi1988okamoto@gmail.com>
2015-01-11 19:06:33 +0900

    ex11fin mrbearing

--- src/com/tasktoys/java8ws/mrbearing/ch9/ex11/CreditNumberSearcher.java
+++ src/com/tasktoys/java8ws/mrbearing/ch9/ex11/CreditNumberSearcher.java
@@ -1,18 +1,41 @@
 package com.tasktoys.java8ws.mrbearing.ch9.ex11;
 
 import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class CreditNumberSearcher {
 
-  public CreditNumberSearcher() {
-    // TODO 自動生成されたコンストラクター・スタブ
-  }
+  private static final String CMD_FINDSTR = "findstr";
+  private static final String REGXP = "\"[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]-[0-9]\"";
+  private static final Path OUTPUT_PATH = Paths.get("out/ch9_11credits.txt");
 
-  public static void main(String[] args) throws InterruptedException, IOException {
-    ProcessBuilder bulider = new ProcessBuilder("findstr");
-    bulider.inheritIO();
-    bulider.start().waitFor();
+  public static void main(String[] args) throws InterruptedException,
+      IOException {
+    String homePath = System.getProperty("user.home") + "\\test\\test.txt";
+
+    //System.out.println(homePath);
+    //System.out.println(OUTPUT_PATH);
+    try{
+    Files.createFile(OUTPUT_PATH);
+    }catch(FileAlreadyExistsException ex){
+      
+    }
     
+    ProcessBuilder bulider = new ProcessBuilder(CMD_FINDSTR,
+        "/S",
+        "/I", REGXP,
+        homePath);
+    bulider.redirectOutput(OUTPUT_PATH.toFile());
+    System.out.println(bulider.command());
+    List<String> lines = Files.readAllLines(OUTPUT_PATH);
+    System.out.println("");
+    for(String s : lines) System.out.println(s);
+    Process process = bulider.start();
+    process.waitFor();
 
   }
 

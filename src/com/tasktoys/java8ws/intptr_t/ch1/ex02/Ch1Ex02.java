4deeb33af8a0158b0da648e83b1a9588228b65b8
intptr_t <niwatho.twi@gmail.com>
2015-01-25 22:26:27 +0900

    update chapter1-02

--- src/com/tasktoys/java8ws/intptr_t/ch1/ex02/Ch1Ex02.java
+++ src/com/tasktoys/java8ws/intptr_t/ch1/ex02/Ch1Ex02.java
@@ -1,30 +1,47 @@
 package com.tasktoys.java8ws.intptr_t.ch1.ex02;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Ch1Ex02 {
 	public static void main(String[] args) {
 		File file = new File("./");
-		File[] dirs1, dirs2;
+		ArrayList<File> subdirs1, subdirs2;
+		
+		subdirs1 = new ArrayList<File>();
+		subdirs2 = new ArrayList<File>();
 		
 		// lambda version
 		System.out.println("lambda:");
-		dirs1 = file.listFiles( name -> name.isDirectory() );
-		Stream.of(dirs1).forEach(System.out::println);
-		
+		enumDirsWithLambda( subdirs1,file.listFiles(name->name.isDirectory()) );
+		subdirs1.stream().forEach(System.out::println);
+
 		// method reference
 		System.out.println("method reference:");
-		dirs2 = file.listFiles(File::isDirectory);
-		Stream.of(dirs2).forEach(System.out::println);
+		enumDirsWithMehodReference( subdirs2,file.listFiles(File::isDirectory) );
+		subdirs2.stream().forEach(System.out::println);
 		
 		// check matching all
 		boolean match = true;
-		for( File dir1 : dirs1 ) {
-			match &= Arrays.asList(dirs2).contains(dir1);
+		for( File dir1 : subdirs1 ) {
+			match &= subdirs2.contains(dir1);
 			if(!match){ break; }
 		}
 		System.out.println("match?: " + match);
+	}	
+	
+	static void enumDirsWithLambda( List<File> list, File[] dirs ){
+		for( File dir : dirs ) {
+			list.add(dir);
+			enumDirsWithLambda(list,dir.listFiles(name->name.isDirectory()));
+		}
+	}
+
+	static void enumDirsWithMehodReference( List<File> list, File[] dirs ) {
+		for( File dir : dirs ) {
+			list.add(dir);
+			enumDirsWithMehodReference(list,dir.listFiles(File::isDirectory));
+		}		
 	}
 }

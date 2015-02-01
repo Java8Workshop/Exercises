497de359ce5a3956611a2db47bf9de0fcc7da0bb
takumi Okamoto <takumi1988okamoto@gmail.com>
2015-02-01 21:20:59 +0900

    ex3

---
+++ src/com/tasktoys/java8ws/mrbearing/ch1/ex3/ExtensionFinder.java
@@ -0,0 +1,18 @@
+package com.tasktoys.java8ws.mrbearing.ch1.ex3;
+
+public class ExtensionFinder {
+
+  public ExtensionFinder() {
+    // TODO 自動生成されたコンストラクター・スタブ
+  }
+
+  public static void main(String[] args) {
+    String dirString = "./";
+    String extString = "txt";
+    
+    //File directory = new File()；
+     
+
+  }
+
+}

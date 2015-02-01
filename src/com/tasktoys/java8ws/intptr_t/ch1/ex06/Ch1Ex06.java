8ddff302f1843068a631efb5b9714e6a8340fdf8
intptr_t <niwatho.twi@gmail.com>
2015-02-01 22:40:20 +0900

    update chapter1-06

--- src/com/tasktoys/java8ws/intptr_t/ch1/ex06/Ch1Ex06.java
+++ src/com/tasktoys/java8ws/intptr_t/ch1/ex06/Ch1Ex06.java
@@ -1,5 +1,7 @@
 package com.tasktoys.java8ws.intptr_t.ch1.ex06;
 
+import java.util.concurrent.Callable;
+
 interface RunnableEx {
 	public void run() throws Exception;
 }
@@ -10,6 +12,12 @@ public class Ch1Ex06 {
 			System.out.println("Zzz");
 			Thread.sleep(1000);
 		})).start();
+		
+		new Thread(uncheckCallable(() -> {
+			System.out.println("Xxx");
+			Thread.sleep(1000);
+			return null; // Callable<T> must return any reference.
+		})).start();
 	}
 
 	static Runnable uncheck(RunnableEx runner){
@@ -17,6 +25,16 @@ public class Ch1Ex06 {
 			try {
 				runner.run();
 			} catch(Throwable t) {
+				// kill them all exceptions!
+			}
+		};
+	}
+
+	static Runnable uncheckCallable(Callable<Void> caller){
+		return () -> {
+			try {
+				caller.call();
+			} catch(Throwable r) {
 				// kill them all exceptions!
 			}
 		};

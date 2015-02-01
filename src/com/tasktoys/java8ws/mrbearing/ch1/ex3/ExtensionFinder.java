1c37971822b7da9a34d9a1457e628b95a3e79692
takumi Okamoto <takumi1988okamoto@gmail.com>
2015-02-01 22:51:28 +0900

    ex3　clear

--- src/com/tasktoys/java8ws/mrbearing/ch1/ex3/ExtensionFinder.java
+++ src/com/tasktoys/java8ws/mrbearing/ch1/ex3/ExtensionFinder.java
@@ -1,18 +1,37 @@
 package com.tasktoys.java8ws.mrbearing.ch1.ex3;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ExtensionFinder {
 
-  public ExtensionFinder() {
-    // TODO 自動生成されたコンストラクター・スタブ
+  public static List<File> listFileByExteinsion(File directory, String ext) {
+    List<File> resultFileList = new ArrayList<>();
+    String[] fileStringArray = directory.list((File file , String string ) -> {
+      return string.endsWith(ext);
+    });
+    for(String path : fileStringArray){
+      resultFileList.add(Paths.get(path).toFile());
+    }
+    
+    return resultFileList;
   }
 
   public static void main(String[] args) {
-    String dirString = "./";
-    String extString = "txt";
-    
-    //File directory = new File()；
-     
+    Path currentPath = Paths.get("./", "src","com","tasktoys","java8ws",
+        "mrbearing","ch1","ex1");
+    String ext = "java";
+    File dir = currentPath.toFile();
 
+    
+    List<File> resultFileList = listFileByExteinsion(dir, ext);
+    
+    resultFileList.forEach(System.out::println);
+    
   }
 
 }

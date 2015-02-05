7c4ae2612bb3dff0784846e13377508bd1238e85
hosoai <hosoai@qito.kyushu-u.ac.jp>
2015-02-05 22:59:50 +0900

    add ch9.ex01 　例外周りはほんとに頭が痛い，，．close時Exceptionのテストってどうやればいいんだろう．．

---
+++ src/com/tasktoys/java8ws/hosoai/ch9/ex01/WithoutTryWithResources.java
@@ -0,0 +1,73 @@
+package com.tasktoys.java8ws.hosoai.ch9.ex01;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class WithoutTryWithResources {
+	public void withoutTryWithResources(String inputFile, String outputFile){
+		Scanner in = null;
+		PrintWriter out = null;
+		try {
+			in = new Scanner(inputFile);
+			out = new PrintWriter(outputFile);
+			while(in.hasNext()){
+				out.println(in.next().toLowerCase());
+			}
+		} catch (IOException e) {
+			try{
+				if(in!=null){
+					in.close();
+				}
+			}catch(Exception e1){
+				System.err.println("Cannnot Close input file");
+				throw e1;
+			}finally{
+				in = null;
+			}
+			try{
+				if(out!=null){
+					out.close();
+				}
+			}catch(Exception e2){
+				System.err.println("Cannnot Close output file");
+				throw e2;
+			}finally{
+				out = null;
+			}
+			return;
+		} finally {
+			try{
+				if(in!=null){
+					in.close();
+				}
+			}catch(Exception e){
+				System.err.println("Cannnot Close input file");
+				throw e;
+			}finally{
+				in = null;
+			}
+			try{
+				if(out!=null){
+					out.close();
+				}
+			}catch(Exception e){
+				System.err.println("Cannnot Close output file");
+				throw e;
+			}finally{
+				out = null;
+			}
+		}
+	}
+	
+	public void tryWithResources(String inputFile, String outputFile){
+		try(Scanner in = new Scanner(inputFile); PrintWriter out = new PrintWriter(outputFile)){
+			while(in.hasNext()){
+				out.println(in.next().toLowerCase());
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ test/com/tasktoys/java8ws/hosoai/ch9/ex01/WithoutTryWithResourcesTest.java
@@ -0,0 +1,94 @@
+package com.tasktoys.java8ws.hosoai.ch9.ex01;
+
+import static org.junit.Assert.*;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WithoutTryWithResourcesTest {
+	WithoutTryWithResources target;
+	@Before
+	public void setUp() throws Exception {
+		target = new WithoutTryWithResources();
+	}
+
+	@Test
+	public void testValidInandOut() {
+		try{
+			target.withoutTryWithResources("out/README", "out/output.txt");
+		}catch(Exception e){
+			fail();
+		}
+	}
+
+	@Test
+	public void testInvalidIn() {
+		try{
+			target.withoutTryWithResources("", "out/output.txt");
+		}catch(Exception e){
+			fail();
+		}
+	}
+
+	@Test
+	public void testInvalidOut() {
+		try{
+			target.withoutTryWithResources("out/README", "");
+		}catch(Exception e){
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	@Test
+	public void testInvalidInOut() {
+		try{
+			target.withoutTryWithResources("", "");
+		}catch(Exception e){
+			e.printStackTrace();
+			fail();
+		}
+	}
+	@Test
+	public void testValidInandOutJava7() {
+		try{
+			target.tryWithResources("out/README", "out/output.txt");
+		}catch(Exception e){
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	@Test
+	public void testInvalidInJava7() {
+		try{
+			target.tryWithResources("", "out/output.txt");
+		}catch(Exception e){
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	@Test
+	public void testInvalidOutJava7() {
+		try{
+			target.tryWithResources("out/README", "");
+		}catch(Exception e){
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	@Test
+	public void testInvalidInOutJava7() {
+		try{
+			target.tryWithResources("", "");
+		}catch(Exception e){
+			e.printStackTrace();
+			fail();
+		}
+	}
+}

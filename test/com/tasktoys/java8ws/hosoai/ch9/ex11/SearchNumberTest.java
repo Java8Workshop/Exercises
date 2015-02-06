597c7776ccd8f944e7627724026c06cd0396928a
hosoai <hosoai@qito.kyushu-u.ac.jp>
2015-02-06 22:59:33 +0900

    add ch9.ex11 　楽しい問題出しますね~，Win環境なもんでGrepがいません．Dirで取ってゴリ押しで探してます．

---
+++ test/com/tasktoys/java8ws/hosoai/ch9/ex11/SearchNumberTest.java
@@ -0,0 +1,19 @@
+package com.tasktoys.java8ws.hosoai.ch9.ex11;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SearchNumberTest {
+	SearchNumber target;
+	@Before
+	public void setUp() throws Exception {
+		target = new SearchNumber();
+	}
+
+	@Test
+	public void test() {
+		target.search();
+	}
+}

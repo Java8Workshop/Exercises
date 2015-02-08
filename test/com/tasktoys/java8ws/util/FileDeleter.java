2e8dfaf7555fe611bf35f9224863e99ad159560f
LagunaPresa <LagunaPresa@192.168.1.103>
2015-02-08 23:03:03 +0900

    update util

--- test/com/tasktoys/java8ws/util/FileDeleter.java
+++ test/com/tasktoys/java8ws/util/FileDeleter.java
@@ -3,18 +3,27 @@ package com.tasktoys.java8ws.util;
 import static org.junit.Assert.fail;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Arrays;
+import java.util.stream.Stream;
 
 public final class FileDeleter {
 
+	private static final FileFilter ACCEPT_ALL = f -> true;
+
 	public static void deleteIfExists(File root) {
+		deleteIfExists(root, ACCEPT_ALL);
+	}
+
+	public static void deleteIfExists(File root, FileFilter filter) {
 		if (root.isDirectory()) {
-			Arrays.asList(root.listFiles()).forEach(FileDeleter::deleteIfExists);
+			Stream.of(root.listFiles()).forEach(f -> FileDeleter.deleteIfExists(f, filter));
 		}
 		try {
-			Files.deleteIfExists(root.toPath());
+			if (filter.accept(root)) {
+				Files.deleteIfExists(root.toPath());
+			}
 		} catch (IOException e) {
 			fail();
 		}

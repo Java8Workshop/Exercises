097f3f5ee61f9d5386088b896f9666a001f06d4a
hosoai <hosoai@qito.kyushu-u.ac.jp>
2015-02-08 21:29:13 +0900

    add ch1.ex03  ラムダの中にいろいろ書きたくないなぁ，，現状拡張子がないと死にます．できれば一度別のラムダではねてから操作したいなぁ．

---
+++ test/com/tasktoys/java8ws/hosoai/ch1/ex03/LambdaFileFilterTest.java
@@ -0,0 +1,25 @@
+package com.tasktoys.java8ws.hosoai.ch1.ex03;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LambdaFileFilterTest {
+	LambdaFileFilter target;
+
+	@Before
+	public void setUp() throws Exception {
+		target = new LambdaFileFilter();
+	}
+
+	@Test
+	public void test() {
+		File[] files = target.getFilteredFiles("out", "txt");
+		for(File f : files){
+			System.out.println(f);
+		}
+	}
+}

c235021f4df74ce6191181d7d4e6b572dde293db
takumi Okamoto <takumi1988okamoto@gmail.com>
2015-02-08 21:56:11 +0900

    ex4

---
+++ test/com/tasktoys/java8ws/mrbearing/ch1/ex04/FileObjectSortTest.java
@@ -0,0 +1,22 @@
+package com.tasktoys.java8ws.mrbearing.ch1.ex04;
+
+import static org.junit.Assert.*;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+public class FileObjectSortTest {
+
+  @Test
+  public void testFileSort() {
+    Path currentPath = Paths.get("./", "src","com","tasktoys","java8ws",
+        "mrbearing","ch1","ex1");
+    
+    
+    
+    fail("まだ実装されていません");
+  }
+
+}

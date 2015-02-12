d600dbc1276e5fb4812b46ace5616b3c3ad16dbc
LagunaPresa <LagunaPresa@LagunaPresa-MBA.local>
2015-02-12 23:37:36 +0900

    update ch01-ex06

--- src/com/tasktoys/java8ws/lagunapresa/ch1/ex06/RunnableEx.java
+++ src/com/tasktoys/java8ws/lagunapresa/ch1/ex06/RunnableEx.java
@@ -1,5 +1,7 @@
 package com.tasktoys.java8ws.lagunapresa.ch1.ex06;
 
+import java.util.concurrent.Callable;
+
 @FunctionalInterface
 public interface RunnableEx {
 
@@ -22,6 +24,13 @@ public interface RunnableEx {
             Thread.sleep(1000L);
         })).start();
         // catch 必要なし
+    }
+
+    static Callable<Void> getNothingReturns() {
+        // Callable<Void> の #call メソッドのシグネチャは call : void ではなく call : java.lang.Void
+        // つまり戻り値を持たないのではなく java.lang.Void のインスタンスか null を
+        // 返さなくてはならない。（と思う）
+        return null;
     }
 
 }

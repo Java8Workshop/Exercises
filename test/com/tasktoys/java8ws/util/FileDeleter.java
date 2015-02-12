710d2d2a05a30bdd1aa40dd880a51be31a65aa43
LagunaPresa <LagunaPresa@LagunaPresa-MBA.local>
2015-02-12 22:39:30 +0900

    bugfix:FileDeleter

--- test/com/tasktoys/java8ws/util/FileDeleter.java
+++ test/com/tasktoys/java8ws/util/FileDeleter.java
@@ -1,38 +1,39 @@
 package com.tasktoys.java8ws.util;
 
-import static org.junit.Assert.fail;
-
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.stream.Stream;
 
+import static org.junit.Assert.fail;
+
 public final class FileDeleter {
 
-	private static final FileFilter ACCEPT_ALL = f -> true;
+    private static final FileFilter ACCEPT_ALL = f -> true;
 
-	public static void deleteIfExists(File root) {
-		deleteIfExists(root, ACCEPT_ALL);
-	}
+    public static void deleteIfExists(File root) {
+        deleteIfExists(root, ACCEPT_ALL);
+    }
 
-	public static void deleteIfExists(File root, FileFilter filter) {
-		if (root.isDirectory()) {
-			Stream.of(root.listFiles()).forEach(f -> FileDeleter.deleteIfExists(f, filter));
-		} else {
-			try {
-				if (filter.accept(root)) {
-					Files.deleteIfExists(root.toPath());
-				}
-			} catch (IOException e) {
-				fail(e.toString());
-			}
-		}
-	}
+    public static void deleteIfExists(File root, FileFilter filter) {
+        if (root.isDirectory()) {
+            Stream.of(root.listFiles()).forEach(f -> FileDeleter.deleteIfExists(f, filter));
 
-	// utility class
-	private FileDeleter() {
-		;
-	}
+        }
+        try {
+            if (filter.accept(root)) {
+                Files.deleteIfExists(root.toPath());
+            }
+        } catch (IOException e) {
+            fail(e.toString());
+        }
+
+    }
+
+    // utility class
+    private FileDeleter() {
+        ;
+    }
 
 }

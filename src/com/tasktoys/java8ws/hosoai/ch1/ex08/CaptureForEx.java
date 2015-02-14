1e6adc94a32407dce2a4c345575ff8b648881759
hosoai <hosoai@qito.kyushu-u.ac.jp>
2015-02-14 22:19:40 +0900

    add ch1.ex08 	拡張Forで渡された変数はキャプチャできる， 	普通のforのiはfinalじゃないので，キャプチャできない．って理解でいいのかな． 	もしかしてString[]限定ってことはないよね・・？Object[]とかだと参照先の内容を変えれてしまいそうだけども・・ 	runners.addからrunまでの間に，namesの内容を変更したらどうなるんだろう．

---
+++ src/com/tasktoys/java8ws/hosoai/ch1/ex08/CaptureForEx.java
@@ -0,0 +1,36 @@
+package com.tasktoys.java8ws.hosoai.ch1.ex08;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CaptureForEx {
+	public void callNamesByForEx(){
+		String[] names = {"Peter", "Paul", "Mary"};
+		List<Runnable> runners = new ArrayList<>();
+		for(String name : names){
+			runners.add(()->System.out.println(name));
+		}
+		for(Runnable r :runners){
+			r.run();
+		}
+		runners.clear();
+	}
+	
+	private int i; // クラス変数にすればキャプチャできるが・・・．		
+	private String[] names = {"Peter", "Paul", "Mary"};
+	private List<Runnable> runners = new ArrayList<>();
+	public void callNamesByFor(){
+		//for(int i=0;i<names.length;i++){ // ローカルスコープの変数はコンパイルではねられる．クラス変数にしたら実行時に参照できなくてダメっぽい．
+		for(i=0;i<names.length;i++){
+			runners.add(()->System.out.println(names[i]));
+		}
+		for(Runnable r :runners){
+			r.run();
+		}
+	}
+	
+	public static void main(String[] args) {
+		new CaptureForEx().callNamesByForEx();
+		new CaptureForEx().callNamesByFor();
+	}
+}

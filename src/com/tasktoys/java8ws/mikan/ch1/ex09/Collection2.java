1ad8b71994714a4cc91700f0a2e8a756acda7aaf
mikan <yutakakato@icloud.com>
2015-02-22 23:07:12 +0900

    Added ch1-ex09.

---
+++ src/com/tasktoys/java8ws/mikan/ch1/ex09/Collection2.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright(C) 2014-2015 Java 8 Workshop participants. All rights reserved.
+ * https://github.com/Java8Workshop/About
+ */
+package com.tasktoys.java8ws.mikan.ch1.ex09;
+
+import java.util.Collection;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+/**
+ *
+ * @author mikan
+ */
+public interface Collection2<E> extends Collection<E> {
+
+    void forEachIf(Consumer<E> action, Predicate<E> filter);
+}
---
+++ src/com/tasktoys/java8ws/mikan/ch1/ex09/List2.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright(C) 2014-2015 Java 8 Workshop participants. All rights reserved.
+ * https://github.com/Java8Workshop/About
+ */
+package com.tasktoys.java8ws.mikan.ch1.ex09;
+
+/**
+ *
+ * @author mikan
+ */
+public interface List2<E> extends Collection2<E> {
+
+}

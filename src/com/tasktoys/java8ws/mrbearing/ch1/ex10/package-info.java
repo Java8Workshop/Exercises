b083e770ae06a99222da523c7d4f45474cf2c16b
MrBearing <takumi1988okamoto@gmail.com>
2015-03-01 22:17:47 +0900

    ex10追加

---
+++ src/com/tasktoys/java8ws/mrbearing/ch1/ex10/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * 題意解釈：Collectionsクラスのメソッドを他のクラスに実装するとしたら、
+ * あなたはどのメソッドをどのインタフェースに実装しますか？
+ * それはstaticとdefaultのどちらで実装しますか？
+ *　 
+ *　checkedMap等のcheckedXXメソッドはstatcメソッドとしてXXのインタフェースに実装する。
+ * copyメソッドはListインタフェースにdefaultメソッドとして実装したい。
+ * 
+ */
+/**
+ * @author Takumi
+ *
+ */
+package com.tasktoys.java8ws.mrbearing.ch1.ex10;

d5a2e8cc8f5612ea511a137b3537cac85346aacd
hosoai <hosoai@qito.kyushu-u.ac.jp>
2015-03-29 22:41:57 +0900

    fix 結果を貼りましたー．

--- src/com/tasktoys/java8ws/hosoai/ch2/ex04/TypeofIntArrayStream.java
+++ src/com/tasktoys/java8ws/hosoai/ch2/ex04/TypeofIntArrayStream.java
@@ -3,6 +3,12 @@ package com.tasktoys.java8ws.hosoai.ch2.ex04;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+/*
+ * Stream.of(values)
+ *  -> class java.util.stream.ReferencePipeline$Head
+ * IntSream.of(values)
+ *  -> class java.util.stream.IntPipeline$Head
+ */
 public class TypeofIntArrayStream {
 	public static void main(String[] args) {
 		int[] values = new int[]{1,4,9,16};

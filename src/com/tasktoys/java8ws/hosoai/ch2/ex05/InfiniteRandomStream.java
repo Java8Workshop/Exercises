8d0926e86556f8de91f9ce3f753a343c63faae17
hosoai <hosoai@qito.kyushu-u.ac.jp>
2015-03-29 22:42:23 +0900

    fix 結果を貼りましたー

--- src/com/tasktoys/java8ws/hosoai/ch2/ex05/InfiniteRandomStream.java
+++ src/com/tasktoys/java8ws/hosoai/ch2/ex05/InfiniteRandomStream.java
@@ -2,6 +2,22 @@ package com.tasktoys.java8ws.hosoai.ch2.ex05;
 
 import java.util.stream.Stream;
 
+/*
+ * X0 = 100L
+ * Results
+ *  100
+ *  2521490391711
+ *  -253956958586946
+ *  -256932786786319
+ *  -262564217229400
+ *  94254738963091
+ *  104947696648866
+ *  243277075933957
+ *  84767341874988
+ *  -139136472121913
+ * 
+ */
+
 public class InfiniteRandomStream {
 	public static void main(String[] args) {
 		LinearCongruentialGenerator lcg = new LinearCongruentialGenerator();

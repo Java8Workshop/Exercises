14d486d6e096135cb13a39110dfd81ebcf4b7b90
YuichiroSato <physics.for.high.school.students@gmail.com>
2015-03-29 22:12:46 +0900

    add Thread implementation

--- src/com/tasktoys/java8ws/sato/ch2/ex03/Ex03.java
+++ src/com/tasktoys/java8ws/sato/ch2/ex03/Ex03.java
@@ -9,25 +9,46 @@ import java.util.List;
 
 public class Ex03 {
 
+	/*
+	 * exp2
+	 * parallel
+	 * 2ms
+	 * count 1946
+	 * nonParallel
+	 * 3ms
+	 * count 1946
+	 * by Thread
+	 * 20ms
+	 * count 1946
+	 * 
+	 */
+	
 	public static void main(String[] args) {
 		List<String> words = getWords();
 		System.out.println("parallel");
-		long start = System.currentTimeMillis();
-		long c = parallel(words);
-		System.out.println((System.currentTimeMillis() - start) + "ms");
-		System.out.println("count " + c);
+		parallel(words);
 		
 		System.out.println("nonParallel");
-		long start2 = System.currentTimeMillis();
-		long c2 = nonParallel(words);
-		System.out.println((System.currentTimeMillis() - start2) + "ms");
-		System.out.println("count" + c2);
+		nonParallel(words);
+		
+		System.out.println("by Thread");
+		thread(words);
+		
+		System.out.println("\nexp2");
+		System.out.println("parallel");
+		parallel(words);
+		
+		System.out.println("nonParallel");
+		nonParallel(words);
+		
+		System.out.println("by Thread");
+		thread(words);
 	}
 	
 	public static List<String> getWords() {
 		String content = "";
 		try {
-			content = new String(Files.readAllBytes(Paths.get("out/war_and_peace.txt")), StandardCharsets.UTF_8);
+			content = new String(Files.readAllBytes(Paths.get("out/pg2600.txt")), StandardCharsets.UTF_8);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -35,12 +56,44 @@ public class Ex03 {
         return Arrays.asList(content.split("[\\P{L}]+"));
 	}
 	
-	public static long parallel(List<String> words) {
-		return words.parallelStream().filter(s -> s.length() > 12).count();
+	public static void parallel(List<String> words) {
+		long start = System.currentTimeMillis();
+		long c = words.parallelStream().filter(s -> s.length() > 12).count();
+		System.out.println((System.currentTimeMillis() - start) + "ms");
+		System.out.println("count " + c);
 	}
 	
-	public static long nonParallel(List<String> words) {
-		return words.stream().filter(s -> s.length() > 12).count();
+	public static void nonParallel(List<String> words) {
+		long start = System.currentTimeMillis();
+		long c = words.stream().filter(s -> s.length() > 12).count();
+		System.out.println((System.currentTimeMillis() - start) + "ms");
+		System.out.println("count " + c);
 	}
 
+	public static void thread(List<String> words) {
+		long start = System.currentTimeMillis();
+		
+		int segment = words.size() / 100;
+		int fromIndex = 0;
+		int toIndex = segment;
+		
+		MyThread[] tarray = new MyThread[101];
+		for (int i = 0; i < 100; i++) {
+			tarray[i] = new MyThread(words.subList(fromIndex, toIndex));
+			tarray[i].start();
+			fromIndex += segment;
+			toIndex += segment;
+		}
+		MyThread last = new MyThread(words.subList(fromIndex, words.size()));
+		last.start();
+		
+		long total = 0;
+		for (int i = 0; i < 100; i++) {
+			total += tarray[i].getResult();
+		}
+		total += last.getResult();
+		
+		System.out.println((System.currentTimeMillis() - start) + "ms");
+		System.out.println("count " + total);
+	}
 }

7aba84e200a4b6cf82329dfc019688654ab29080
YuichiroSato <physics.for.high.school.students@gmail.com>
2015-03-29 23:02:41 +0900

    ch02 ex04

---
+++ src/com/tasktoys/java8ws/sato/ch2/ex04/Main.java
@@ -0,0 +1,34 @@
+package com.tasktoys.java8ws.sato.ch2.ex04;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] values = new int[] { 1, 4, 9, 16 };
+		
+		System.out.println(Stream.of(values));
+		
+		System.out.println("stream of");
+		Stream.of(values).forEach(v -> {
+			for (int i = 0 ; i < v.length; i++) {
+				System.out.print(v[i] + " ");
+				}
+			});
+		System.out.println("\n");
+		
+		System.out.println("arrays as list");
+		Arrays.asList(values).stream().forEach(v -> {
+			for (int i = 0 ; i < v.length; i++) {
+				System.out.print(v[i] + " ");
+				}
+			});
+		System.out.println("\n");
+		
+		System.out.println("arrays stream");
+		Arrays.stream(values).forEach(v -> System.out.print(v + " "));
+	}
+
+}

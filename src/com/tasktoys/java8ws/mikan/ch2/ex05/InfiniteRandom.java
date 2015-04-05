cdd9350fe50e6f6d2b5205e6ca62f30f23127628
mikan <yutakakato@icloud.com>
2015-04-05 22:09:17 +0900

    Updated ch2-ex05.

---
+++ src/com/tasktoys/java8ws/mikan/ch2/ex05/InfiniteRandom.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright(C) 2014-2015 Java 8 Workshop participants. All rights reserved.
+ * https://github.com/Java8Workshop/Exercises
+ */
+package com.tasktoys.java8ws.mikan.ch2.ex05;
+
+import java.util.stream.Stream;
+
+/**
+ *
+ * @author mikan
+ */
+public class InfiniteRandom {
+
+    public static void main(String[] args) {
+        Stream<Long> stream = Stream.iterate(0L, n -> random(25214903917L, 11, 2^48, n));
+        stream.limit(10).forEach(System.out::println);
+    }
+
+    private static long random(long a, long c, long m, long x) {
+        return (a * x + c) % m;
+    }
+}

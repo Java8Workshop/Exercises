7b443a1b54ccf4a204f192d70dad7017da0c2adb
hosoai <hosoai@qito.kyushu-u.ac.jp>
2015-04-05 21:47:37 +0900

    fix インスタンス作る必要もなさそうなので，staticに変更

--- src/com/tasktoys/java8ws/hosoai/ch2/ex05/InfiniteRandomStream.java
+++ src/com/tasktoys/java8ws/hosoai/ch2/ex05/InfiniteRandomStream.java
@@ -20,17 +20,16 @@ import java.util.stream.Stream;
 
 public class InfiniteRandomStream {
 	public static void main(String[] args) {
-		LinearCongruentialGenerator lcg = new LinearCongruentialGenerator();
-		Stream.iterate(100L, lcg::getNext).limit(10).forEach(System.out::println);;
+		Stream.iterate(100L, LinearCongruentialGenerator::getNext).limit(10).forEach(System.out::println);;
 	}	
 }
 
 class LinearCongruentialGenerator{
-	long a = 25214903917L;
-	long c = 11;
-	long m = (long)Math.pow(2, 48);
+	static long a = 25214903917L;
+	static long c = 11;
+	static long m = (long)Math.pow(2, 48);
 		
-	public long getNext(long x){
+	public static long getNext(long x){
 		return (a * x + c) % m;
 	}
 }

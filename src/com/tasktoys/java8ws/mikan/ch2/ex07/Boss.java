23c10c3b88595584d490e9055f54d535f0ea93a5
mikan <yutakakato@icloud.com>
2015-04-19 23:10:43 +0900

    Updated ch2-ex07 with spliterator method.

--- src/com/tasktoys/java8ws/mikan/ch2/ex07/Boss.java
+++ src/com/tasktoys/java8ws/mikan/ch2/ex07/Boss.java
@@ -13,11 +13,19 @@ import java.util.stream.Stream;
 public class Boss {
 
     public static void main(String[] args) {
-        Stream<String> infinite = Stream.generate(() -> "yeah!").parallel();
-        System.out.println("result:" + isFinite(infinite));
+        Stream<String> finite = Stream.of("yeah!", "yahoo!");
+        System.out.println("finite result:\t\t\t" + isFinite(finite));
+        Stream<String> infinite = Stream.generate(() -> "yeah!");
+        System.out.println("infinite result:\t\t" + isFinite(infinite));
+        Stream<String> infiniteParallel = Stream.generate(() -> "yeah!").parallel();
+        System.out.println("infiniteParallel result:\t" + isFinite(infiniteParallel));
     }
 
     public static <T> boolean isFinite(Stream<T> stream) {
+        return stream.spliterator().getExactSizeIfKnown() != -1;
+    }
+
+    public static <T> boolean isFiniteHungUp(Stream<T> stream) {
         return stream.count() >= 0; // DO NOT RUN!
     }
 }

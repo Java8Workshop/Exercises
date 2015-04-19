5307c352da92322f064b862abb30e8bb3df3f903
MrBearing <takumi1988okamoto@gmail.com>
2015-04-19 21:51:22 +0900

    fin ex5 okamoto

--- src/com/tasktoys/java8ws/mrbearing/ch2/ex5/InfStream.java
+++ src/com/tasktoys/java8ws/mrbearing/ch2/ex5/InfStream.java
@@ -1,11 +1,22 @@
 package com.tasktoys.java8ws.mrbearing.ch2.ex5;
 
+import java.util.stream.Stream;
+
 public class InfStream {
 
-  
-  
-  public static void main(String[] args) {
     
-  }
+    public static Stream<Long> createInfintStream(long a,long c,long m, long seed){
+        return Stream.iterate(seed, (n -> (a+n+c)%m ));
+    }
+    
+    public static void main(String[] args) {
+        
+        Stream<Long> infstream = createInfintStream(
+                25214903917L, 11,
+                (long) Math.pow(2, 48),
+                System.currentTimeMillis());
+        System.out.println("valuse...");
+        infstream.limit(5).forEach(System.out::println);
+    }
 
 }

f9a0874d5d08dad15d86f54dd99a2a826421a0e0
LagunaPresa <sunspotactivity@gmail.com>
2015-05-03 22:25:10 +0900

    [WIP] Add ch02-ex08

---
+++ src/com/tasktoys/java8ws/lagunapresa/ch2/ex08/Zipper.java
@@ -0,0 +1,11 @@
+package com.tasktoys.java8ws.lagunapresa.ch2.ex08;
+
+import java.util.stream.Stream;
+
+public class Zipper {
+
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
+        return null;// TODO
+    }
+
+}

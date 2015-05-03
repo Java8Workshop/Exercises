6979e3b97cffb6f69c88ff8f30b25cdfe2ae8e59
YuichiroSato <physics.for.high.school.students@gmail.com>
2015-05-04 05:02:16 +0900

    ch3 ex6

---
+++ src/com/tasktoys/java8ws/sato/ch3/ex6/ImageUtil.java
@@ -0,0 +1,69 @@
+package com.tasktoys.java8ws.sato.ch3.ex6;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageOutputStream;
+
+public class ImageUtil {
+	
+	public static Image getImage(String path) {
+		File f = new File(path);
+		Image img = null;
+		if (f.exists()) {
+			FileInputStream in;
+			try {
+				in = new FileInputStream(f);
+				img = new Image(in);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		return img;
+	}
+	
+	private static BufferedImage imageToBufferedImage(Image img) {
+		int w = (int)img.getWidth();
+		int h = (int)img.getHeight();
+		int[] rgb = new int[w * h];
+		int i = 0;
+		for (int y = 0; y < h; y++) {
+			for (int x = 0; x < w; x++) {
+				rgb[i] = toRGB(img.getPixelReader().getColor(x, y));
+				i++;
+			}
+		}
+		BufferedImage bimg = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+		bimg.setRGB(0, 0, w, h, rgb, 0, w);
+		return bimg;
+	}
+	
+	private static int toRGB(Color c) {
+		int a = (int)(c.getBrightness() * 255);
+		int r = (int)(c.getRed() * 255);
+		int g = (int)(c.getGreen() * 255);
+		int b = (int)(c.getBlue() * 255);
+		return (a<<24) | (r<<16) | (g<<8) | b;
+	}
+	
+	public static void outputImage(Image img, String path, String format) {
+		outputImage(imageToBufferedImage(img), path, format);
+	}
+	
+	public static void outputImage(BufferedImage img, String path, String format) {
+		try {
+			ImageOutputStream os = ImageIO.createImageOutputStream(new FileOutputStream(new File(path)));
+			ImageIO.write(img, format, os);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}

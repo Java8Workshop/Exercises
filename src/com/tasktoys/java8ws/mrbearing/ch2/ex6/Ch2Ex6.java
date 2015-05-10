87233af158a15eb2c0bcefecf330f62b98545de1
MrBearing <takumi1988okamoto@gmail.com>
2015-05-10 21:04:45 +0900

    f

---
+++ src/com/tasktoys/java8ws/mrbearing/ch2/ex6/Ch2Ex6.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright(C) 2014-2015 Java 8 Workshop participants. All rights reserved.
+ * https://github.com/Java8Workshop/Exercises
+ */
+package com.tasktoys.java8ws.mrbearing.ch2.ex6;
+
+/**
+ *
+ * @author MrBearing
+ */
+public class Ch2Ex6 {
+    public static void main(String[] args){
+        
+    }
+    
+}

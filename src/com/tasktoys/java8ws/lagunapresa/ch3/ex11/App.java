79a86e4b434d550f14d07a1f1992d70f56dbc6d4
LagunaPresa <sunspotactivity@gmail.com>
2015-05-22 02:32:09 +0900

    Add ch3-ex11

--- src/com/tasktoys/java8ws/lagunapresa/ch3/ex11/App.java
+++ src/com/tasktoys/java8ws/lagunapresa/ch3/ex11/App.java
@@ -1,5 +1,6 @@
-package com.tasktoys.java8ws.lagunapresa.ch3.ex05;
+package com.tasktoys.java8ws.lagunapresa.ch3.ex11;
 
+import com.tasktoys.java8ws.lagunapresa.ch3.ex05.ImagesApp;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
@@ -11,6 +12,7 @@ import static com.tasktoys.java8ws.lagunapresa.ch3.ex05.ColorTransformation.tran
 public class App extends ImagesApp {
 
     private static final double BORDER_WIDTH = 10.0;
+    private static final double BRIGHTNESS_FACTOR = 1.5;
     private static final Color GRAY = new Color(0.3, 0.3, 0.3, 1.0);
     private static final String IMAGE_URL = "http://mnewsplus.blog7.mmm.me/imgs/m/mnewsplus/09aa3757.jpg";
 
@@ -27,10 +29,13 @@ public class App extends ImagesApp {
     protected List<Image> defineImages() {
         Image image = new Image(IMAGE_URL);
         return Arrays.asList(image, transform(image,
-                (x, y, c) -> {
-                    double w = image.getWidth(), h = image.getHeight();
-                    return (x <= BORDER_WIDTH || w - BORDER_WIDTH <= x || y <= BORDER_WIDTH || h - BORDER_WIDTH <= y) ? GRAY : c;
-                }
+                ColorTransformers.combine(
+                        ColorTransformers.from(c -> c.deriveColor(0.0, 1.0, BRIGHTNESS_FACTOR, 1.0)),
+                        (x, y, c) -> {
+                            double w = image.getWidth(), h = image.getHeight();
+                            return (x <= BORDER_WIDTH || w - BORDER_WIDTH <= x || y <= BORDER_WIDTH || h - BORDER_WIDTH <= y) ? GRAY : c;
+                        }
+                )
         ));
     }
 

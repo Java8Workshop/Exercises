466b25912fb4b3b483beb6bcf80aa2208774ba3b
LagunaPresa <sunspotactivity@gmail.com>
2015-05-22 08:57:08 +0900

    Refactor ch3-ex11

--- src/com/tasktoys/java8ws/lagunapresa/ch3/ex05/App.java
+++ src/com/tasktoys/java8ws/lagunapresa/ch3/ex05/App.java
@@ -10,12 +10,13 @@ import static com.tasktoys.java8ws.lagunapresa.ch3.ex05.ColorTransformation.tran
 
 public class App extends ImagesApp {
 
-    private static final double BORDER_WIDTH = 10.0;
-    private static final Color GRAY = new Color(0.3, 0.3, 0.3, 1.0);
     private static final String IMAGE_URL = "http://mnewsplus.blog7.mmm.me/imgs/m/mnewsplus/09aa3757.jpg";
 
+    private static final double BORDER_WIDTH = 10.0;
+    private static final Color GRAY = new Color(0.3, 0.3, 0.3, 1.0);
+
     public static void main(String[] args) {
-        App.launch();
+        launch();
     }
 
     @Override
@@ -26,12 +27,16 @@ public class App extends ImagesApp {
     @Override
     protected List<Image> defineImages() {
         Image image = new Image(IMAGE_URL);
-        return Arrays.asList(image, transform(image,
-                (x, y, c) -> {
-                    double w = image.getWidth(), h = image.getHeight();
-                    return (x <= BORDER_WIDTH || w - BORDER_WIDTH <= x || y <= BORDER_WIDTH || h - BORDER_WIDTH <= y) ? GRAY : c;
-                }
-        ));
+        return Arrays.asList(
+            image,
+            transform(image,
+                (x, y, c) -> onBorder(image, BORDER_WIDTH, x, y) ? GRAY : c
+            )
+        );
+    }
+
+    protected final boolean onBorder(Image img, double borderWidth, int x, int y) {
+        return (x <= borderWidth || img.getWidth() - borderWidth <= x || y <= borderWidth || img.getHeight() - borderWidth <= y);
     }
 
 }
--- src/com/tasktoys/java8ws/lagunapresa/ch3/ex11/App.java
+++ src/com/tasktoys/java8ws/lagunapresa/ch3/ex11/App.java
@@ -1,6 +1,5 @@
 package com.tasktoys.java8ws.lagunapresa.ch3.ex11;
 
-import com.tasktoys.java8ws.lagunapresa.ch3.ex05.ImagesApp;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
@@ -8,16 +7,19 @@ import java.util.Arrays;
 import java.util.List;
 
 import static com.tasktoys.java8ws.lagunapresa.ch3.ex05.ColorTransformation.transform;
+import static com.tasktoys.java8ws.lagunapresa.ch3.ex11.ColorTransformers.combine;
+import static com.tasktoys.java8ws.lagunapresa.ch3.ex11.ColorTransformers.from;
 
-public class App extends ImagesApp {
+public class App extends com.tasktoys.java8ws.lagunapresa.ch3.ex05.App {
 
-    private static final double BORDER_WIDTH = 10.0;
-    private static final double BRIGHTNESS_FACTOR = 1.5;
-    private static final Color GRAY = new Color(0.3, 0.3, 0.3, 1.0);
     private static final String IMAGE_URL = "http://mnewsplus.blog7.mmm.me/imgs/m/mnewsplus/09aa3757.jpg";
 
+    private static final double BRIGHTNESS_FACTOR = 1.5;
+    private static final double BORDER_WIDTH = 10.0;
+    private static final Color GRAY = new Color(0.3, 0.3, 0.3, 1.0);
+
     public static void main(String[] args) {
-        App.launch();
+        launch();
     }
 
     @Override
@@ -28,15 +30,15 @@ public class App extends ImagesApp {
     @Override
     protected List<Image> defineImages() {
         Image image = new Image(IMAGE_URL);
-        return Arrays.asList(image, transform(image,
-                ColorTransformers.combine(
-                        ColorTransformers.from(c -> c.deriveColor(0.0, 1.0, BRIGHTNESS_FACTOR, 1.0)),
-                        (x, y, c) -> {
-                            double w = image.getWidth(), h = image.getHeight();
-                            return (x <= BORDER_WIDTH || w - BORDER_WIDTH <= x || y <= BORDER_WIDTH || h - BORDER_WIDTH <= y) ? GRAY : c;
-                        }
+        return Arrays.asList(
+            image,
+            transform(image,
+                combine(
+                    from(c -> c.deriveColor(0.0, 1.0, BRIGHTNESS_FACTOR, 1.0)),
+                    (x, y, c) -> onBorder(image, BORDER_WIDTH, x, y) ? GRAY : c
                 )
-        ));
+            )
+        );
     }
 
 }

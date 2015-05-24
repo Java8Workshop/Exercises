6cc928673bb4557f00af4a39ac077dc65e16dad4
mikan <yutakakato@icloud.com>
2015-05-24 21:58:25 +0900

    Updated ch3-ex10

--- src/com/tasktoys/java8ws/mikan/ch3/ex10/UnaryComposeType.java
+++ src/com/tasktoys/java8ws/mikan/ch3/ex10/UnaryComposeType.java
@@ -30,13 +30,13 @@ public class UnaryComposeType extends Application {
     public void start(Stage primaryStage) throws Exception {
         UnaryOperator<Color> op = Color::brighter;
         Image image = new Image(IMAGE_URL);
+        // ↓同じメソッドがあるが、型が違うのではじかれる (ノミナル型)
+        //
         // 不適合な型: 型変数Vのインスタンスが存在しないので、Function<V,Color>はUnaryOperator<Color>に適合しません
         //  V,T,Rが型変数の場合:
         //    メソッド <V>compose(Function<? super V,? extends T>)で宣言されているVはObjectを拡張します
         //    インタフェース Functionで宣言されているTはObjectを拡張します
         //    インタフェース Functionで宣言されているRはObjectを拡張します
-        // ----
-        // ([Alt]+[Enter]キーを押すとヒントが表示されます)
         // Image brightenedImage = transform(image, op.compose(Color::grayscale));
         primaryStage.setScene(new Scene(new HBox(
                 new ImageView(image)

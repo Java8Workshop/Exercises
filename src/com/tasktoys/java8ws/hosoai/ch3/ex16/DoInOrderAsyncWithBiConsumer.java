189db37c2d18d5f9cafbff71204bc65da95cb297
hosoai <hosoai@qito.kyushu-u.ac.jp>
2015-05-31 21:50:05 +0900

    add ch3.ex16

---
+++ src/com/tasktoys/java8ws/hosoai/ch3/ex16/DoInOrderAsyncWithBiConsumer.java
@@ -0,0 +1,45 @@
+package com.tasktoys.java8ws.hosoai.ch3.ex16;
+
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+
+public class DoInOrderAsyncWithBiConsumer<T> {
+	public void doInOrderAsync(Supplier<T> first,
+			BiConsumer<T, Throwable> handler) {
+		Thread t = new Thread() {
+			public void run() {
+				T result = null;
+				try {
+					result = first.get();
+					handler.accept(result, null);
+				} catch (Throwable t) {
+					handler.accept(null, t);
+				}
+			}
+		};
+		t.start();
+	}
+	
+	public static void main(String[] args) {
+		DoInOrderAsyncWithBiConsumer<String> exceptionHandling = new DoInOrderAsyncWithBiConsumer<String>();
+		
+		exceptionHandling.doInOrderAsync(
+				()->"hello?".substring(10), 
+				(str, u)->{
+					if(str!=null&&u==null){
+						System.out.println("no Exception:"+str);
+					}else{
+						System.out.println("Excetion occured in Suplier");
+					}
+				});
+		exceptionHandling.doInOrderAsync(
+				()->"hello?".substring(0), 
+				(str, u)->{
+					if(str!=null&&u==null){
+						System.out.println("no Exception:"+str);
+					}else{
+						System.out.println("Excetion occured in Suplier");
+					}
+				});
+	}
+}

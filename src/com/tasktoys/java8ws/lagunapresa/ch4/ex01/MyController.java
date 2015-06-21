ed54ff4d0bc7bcf2a76e03315ac671410c0ad9dd
LagunaPresa <sunspotactivity@gmail.com>
2015-06-21 22:21:16 +0900

    Add ch4-ex01

---
+++ src/com/tasktoys/java8ws/lagunapresa/ch4/ex01/MyController.java
@@ -0,0 +1,29 @@
+package com.tasktoys.java8ws.lagunapresa.ch4.ex01;
+
+import javafx.beans.binding.Bindings;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MyController implements Initializable {
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private TextField textField;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        label.textProperty().bind(
+            Bindings.createStringBinding(
+                textField::getText,
+                textField.textProperty()
+            )
+        );
+    }
+}

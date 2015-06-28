487b422a253faf9960307879c4ab2a87a589dbd7
MrBearing <takumi1988okamoto@gmail.com>
2015-06-28 22:06:45 +0900

    fin two param

--- src/com/tasktoys/java8ws/mrbearing/ch4/ex5/Ex5.java
+++ src/com/tasktoys/java8ws/mrbearing/ch4/ex5/Ex5.java
@@ -1,9 +1,7 @@
 package com.tasktoys.java8ws.mrbearing.ch4.ex5;
 
-import static javafx.beans.binding.Bindings.greaterThanOrEqual;
-import static javafx.beans.binding.Bindings.lessThanOrEqual;
-
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
@@ -11,13 +9,8 @@ import javafx.application.Application;
 import javafx.beans.InvalidationListener;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -28,13 +21,18 @@ public class Ex5 extends Application {
 
 	public static <T, R> ObservableValue<R> observe(Function<T, R> f,
 			ObservableValue<T> t) {
+
 		return new ObservableValue<R>() {
+			Map<ChangeListener<? super R>, ChangeListener<T>> changeListenerMap = new HashMap<>();
+			@Override
+			public R getValue() {
+				return f.apply(t.getValue());
+			}
 
 			@Override
 			public void addListener(InvalidationListener arg0) {
 				t.addListener(arg0);
 			}
-
 			@Override
 			public void removeListener(InvalidationListener arg0) {
 				t.removeListener(arg0);
@@ -42,24 +40,20 @@ public class Ex5 extends Application {
 
 			@Override
 			public void addListener(ChangeListener<? super R> arg0) {
-				ChangeListener<T> tlis = (observer, oldV, newV) -> {
-					arg0.changed(this, f.apply(oldV), f.apply(newV));
+				ChangeListener<T> tlistener = (observer, oldVal, newVal) -> {
+					arg0.changed(this, f.apply(oldVal), f.apply(newVal));
 				};
-
-				t.addListener(tlis);
-
-			}
-
-			@Override
-			public R getValue() {
-				// TODO 自動生成されたメソッド・スタブ
-				return null;
+				t.addListener(tlistener);
 			}
 
 			@Override
 			public void removeListener(ChangeListener<? super R> arg0) {
-				// TODO 自動生成されたメソッド・スタブ
-
+				ChangeListener<T> tListener = this.changeListenerMap
+						.getOrDefault(arg0, null);
+				if (tListener != null) {
+					t.removeListener(tListener);
+					this.changeListenerMap.remove(arg0);
+				}
 			}
 		};
 	}
@@ -82,16 +76,11 @@ public class Ex5 extends Application {
 		pane.getChildren().addAll(gauge, outline);
 		smaller.setOnAction(event -> gauge.setWidth(gauge.getWidth() - 10));
 		larger.setOnAction(event -> gauge.setWidth(gauge.getWidth() + 10));
-		/*
-		 * larger.disableProperty().bind( observe(t -> t >= 100 ,
-		 * gauge.widthProperty()));
-		 */
-		/*
-		 * smaller.disableProperty().bind(lessThanOrEqual(gauge.widthProperty(),
-		 * 0));
-		 * larger.disableProperty().bind(greaterThanOrEqual(gauge.widthProperty
-		 * (), 100));
-		 */
+		larger.disableProperty().bind(
+				observe(t -> t.doubleValue() >= 100
+					, gauge.widthProperty()));
+		smaller.disableProperty().bind(
+				observe(t->t.doubleValue()<=0. , gauge.widthProperty()));
 
 		HBox box = new HBox(10);
 		box.getChildren().addAll(smaller, pane, larger);

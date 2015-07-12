bb6a4d39bfe8001d4d2d26dc36c261118479792b
YuichiroSato <physics.for.high.school.students@gmail.com>
2015-07-12 23:18:34 +0900

    ch5 ex2

---
+++ src/com/tasktoys/java8ws/sato/ch5/ex2/Main.java
@@ -0,0 +1,14 @@
+package com.tasktoys.java8ws.sato.ch5.ex2;
+
+import java.time.LocalDate;
+
+public class Main {
+
+	public static void main(String[] args) {
+		LocalDate feb29 = LocalDate.of(2000, 2, 29);
+		System.out.println(feb29);
+		System.out.println("1 year later " + feb29.plusYears(1));
+		System.out.println("4 year later " + feb29.plusYears(4));
+	}
+
+}

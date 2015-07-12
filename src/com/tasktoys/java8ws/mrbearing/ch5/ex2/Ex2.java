962a658ddb7da041804df163645a2f073ef794bc
MrBearing <takumi1988okamoto@gmail.com>
2015-07-12 22:47:40 +0900

    fin 5-2 and 5-3

---
+++ src/com/tasktoys/java8ws/mrbearing/ch5/ex2/Ex2.java
@@ -0,0 +1,25 @@
+package com.tasktoys.java8ws.mrbearing.ch5.ex2;
+
+import java.time.LocalDate;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+
+		LocalDate localdate = LocalDate.of(2000,2,29);
+		// add one year
+		System.out.println(localdate.plusYears(1).toString());
+
+		//add 4year
+		System.out.println(localdate.plusYears(4).toString());
+
+
+		//add 4times 1 year
+		for(int i =0; i<4 ;i++)
+			localdate = localdate.plusYears(1);
+
+		System.out.println(localdate.toString());
+
+	}
+
+}

6a200cdc2e8eab3bee9d6760bd9b40658eefb610
hosoai <junkmiyu@gmail.com>
2015-07-19 21:45:43 +0900

    fix ch5.ex2 : 結果をコード内に表記

--- src/com/tasktoys/java8ws/hosoai/ch5/ex02/AddLocalDate.java
+++ src/com/tasktoys/java8ws/hosoai/ch5/ex02/AddLocalDate.java
@@ -3,6 +3,12 @@ package com.tasktoys.java8ws.hosoai.ch5.ex02;
 import java.time.LocalDate;
 
 public class AddLocalDate {
+	/*
+	 * Result :
+	 * 2001-02-28 : 2000.2.29 + 1 year
+	 * 2004-02-29 : 2000.2.29 + 4 year
+	 * 2004-02-28 : 2000.2.29 + 1 year x 4
+	 */
 	public static void main(String[] args) {
 		LocalDate basedate = LocalDate.of(2000, 2, 29);
 		System.out.println(basedate.plusYears(1) +" : 2000.2.29 + 1 year");

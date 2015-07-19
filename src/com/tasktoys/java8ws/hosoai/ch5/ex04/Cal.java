72f5cd60b57223aa84f2887b4607d97e5c120ff0
hosoai <junkmiyu@gmail.com>
2015-07-19 23:36:39 +0900

    fix ch5.ex04 ： いろいろ間違ってたので修正・・．

--- src/com/tasktoys/java8ws/hosoai/ch5/ex04/Cal.java
+++ src/com/tasktoys/java8ws/hosoai/ch5/ex04/Cal.java
@@ -13,17 +13,19 @@ public class Cal {
 		}catch(Exception e){
 			date = LocalDate.now().withDayOfMonth(1);
 		}
+		System.out.println(paddingRight(date.getYear() + "  "+date.getMonth(), 20));
 		System.out.print(paddingRight("", (date.getDayOfWeek().getValue()-1)*4));
-		
-		while(date.getDayOfMonth() < 31){
+		date = date.minusDays(1);
+		do{
+			date=date.plusDays(1);
 			if(date.getDayOfWeek()==DayOfWeek.SUNDAY){
-				System.err.print(paddingRight(""+date.getDayOfMonth(), 4));								
+//				System.err.print(paddingRight(""+date.getDayOfMonth(), 4));	//たまに出力順がずれちゃうので断念						
+				System.out.print(paddingRight(""+date.getDayOfMonth(), 4));								
 				System.out.println();
 			}else{
 				System.out.print(paddingRight(""+date.getDayOfMonth(), 4));				
-			}			
-			date = date.plusDays(1);
-		}
+			}
+		}while(date.getDayOfMonth() != date.lengthOfMonth());
 	}
 	public static String paddingRight(String str, int num){
 		return String.format("%1$" + num + "s", str);

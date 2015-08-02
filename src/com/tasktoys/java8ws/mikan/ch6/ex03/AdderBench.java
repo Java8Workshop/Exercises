0c8d10411d13ac47e786e834d471ac902cf38443
mikan <yutakakato@icloud.com>
2015-08-02 22:59:31 +0900

    Added ch6-ex03.

---
+++ src/com/tasktoys/java8ws/mikan/ch6/ex03/AdderBench.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright(C) 2014-2015 Java 8 Workshop participants. All rights reserved.
+ * https://github.com/Java8Workshop/Exercises
+ */
+package com.tasktoys.java8ws.mikan.ch6.ex03;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.stream.IntStream;
+
+/**
+ *
+ * @author mikan
+ */
+public class AdderBench {
+
+    public static void main(String[] args) {
+        AdderBench bench = new AdderBench();
+        bench.incrementWithAtomicLong(1000, 100000);
+        bench.incrementWithLongAdder(1000, 100000);
+    }
+
+    public void incrementWithAtomicLong(int threads, int limit) {
+        AtomicLong adder = new AtomicLong();
+        ExecutorService executorService = Executors.newFixedThreadPool(threads);
+        Instant begin = Instant.now();
+        IntStream.range(0, threads).forEach(i -> executorService.submit(() -> IntStream.range(0, limit).forEach(n -> adder.incrementAndGet())));
+        Instant end = Instant.now();
+        System.out.println("AtomicLong: " + Duration.between(begin, end).toMillis() + "msec.");
+    }
+
+    public void incrementWithLongAdder(int threads, int limit) {
+        LongAdder adder = new LongAdder();
+        ExecutorService executorService = Executors.newFixedThreadPool(threads);
+        Instant begin = Instant.now();
+        IntStream.range(0, threads).forEach(i -> executorService.submit(() -> IntStream.range(0, limit).forEach(n -> adder.increment())));
+        Instant end = Instant.now();
+        System.out.println("LongAdder:  " + Duration.between(begin, end).toMillis() + "msec.");
+    }
+
+}

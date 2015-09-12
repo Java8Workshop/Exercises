e4bc10df53038a7b4a21eab26595bc69fa7b30ea
mikan <yutakakato@icloud.com>
2015-09-12 22:07:31 +0900

    Added ch8-ex01, ex02.

---
+++ src/com/tasktoys/java8ws/mikan/ch8/ex01/UnsignedInt.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright(C) 2014-2015 Java 8 Workshop participants. All rights reserved.
+ * https://github.com/Java8Workshop/Exercises
+ */
+package com.tasktoys.java8ws.mikan.ch8.ex01;
+
+/**
+ *
+ * @author mikan
+ */
+public class UnsignedInt {
+
+    private final int value;
+
+    public UnsignedInt() {
+        value = 0;
+    }
+
+    public UnsignedInt(int value) {
+        this.value = value;
+    }
+
+    public int add(int value) {
+        return (int) (Integer.toUnsignedLong(this.value) + Integer.toUnsignedLong(value));
+    }
+
+    public int sub(int value) {
+        return (int) (Integer.toUnsignedLong(this.value) - Integer.toUnsignedLong(value));
+    }
+
+    public int div(int value) {
+        return Integer.divideUnsigned(this.value, value);
+    }
+
+    public int compare(int value) {
+        return Long.compare(Integer.toUnsignedLong(this.value), Integer.toUnsignedLong(value));
+    }
+
+    public int getIntValue() {
+        return value;
+    }
+}
---
+++ src/com/tasktoys/java8ws/mikan/ch8/ex02/NegateExact.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright(C) 2014-2015 Java 8 Workshop participants. All rights reserved.
+ * https://github.com/Java8Workshop/Exercises
+ */
+package com.tasktoys.java8ws.mikan.ch8.ex02;
+
+/**
+ *
+ * @author mikan
+ */
+public class NegateExact {
+
+    public static boolean collectNagateExactError(int value) {
+        try {
+            Math.negateExact(value);
+            return false;
+        } catch (Exception e) {
+            return true;
+        }
+    }
+
+    private NegateExact() {
+
+    }
+}
